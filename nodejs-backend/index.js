1341d52d0a60f90e886772c00b1d9816f29dac4b
Micho-PC <michelhdd27@gmail.com>
2022-07-04 00:53:54 +0300

    initial commit

---
+++ nodejs-backend/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// const userRouter = require('./src/user/index');
+
+//establish connection to Mongodb
+const DB_CONNECT = process.env.DB_CONNECT || "";
+
+mongoose.connect(
+    DB_CONNECT,
+    () => console.log('connected to db')
+);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+//change port to 8080
+app.listen(8080, () => console.log('Server running on 8080'));
