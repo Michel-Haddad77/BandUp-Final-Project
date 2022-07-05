dbf11f19cff66b7441ba5f25155523f93cfd31e4
Micho-PC <michelhdd27@gmail.com>
2022-07-06 01:31:04 +0300

    created 'request to apply' API

--- nodejs-backend/src/bands/controller/bands.js
+++ nodejs-backend/src/bands/controller/bands.js
@@ -1,4 +1,5 @@
 const Band = require('../../../models/Band');
+const Musician = require('../../../models/Musician');
 
 async function getAllBands(req,res){
     try{
@@ -21,7 +22,36 @@ async function getRecentBands(req,res){
     }
 }
 
+async function requestToApply(req,res){
+    try{
+        const {
+            id,
+            musician_id,
+        } = req.body;
+
+        const band = await Band.findById(id);
+        const musician = await Musician.findById(musician_id);
+        
+        //check if the musician wasn't already requested
+        if (!band.requested.includes(musician._id)){
+
+            //add the musician id to the 'requested' array in band
+            band.requested.push(musician); //mongoose takes the document instance and only adds its id to the array
+            band.save();
+    
+            res.status(200).send("Musician was requested successfully");
+            return //to end the function here
+        }
+        
+        res.send("Musician already requested");
+    } catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
+
 module.exports = {
     getAllBands,
     getRecentBands,
+    requestToApply,
 }
--- nodejs-backend/src/bands/index.js
+++ nodejs-backend/src/bands/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
-const { getAllBands, getRecentBands } = require('./controller/bands');
+const { getAllBands, getRecentBands, requestToApply } = require('./controller/bands');
 
 const router = Router();
 
 
 router.get('/all', getAllBands);
 router.get('/recent', getRecentBands);
+router.post('/requested', requestToApply);
 
 
 module.exports = router;
