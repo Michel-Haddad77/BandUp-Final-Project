1ce86a7a5a52525a5211cdc9941c423ef77c1e1b
Micho-PC <michelhdd27@gmail.com>
2022-07-14 00:55:34 +0300

    frontend: updated header style of the bands screen

--- react-native-frontend/App.js
+++ react-native-frontend/App.js
@@ -5,6 +5,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import BandsScreen from './screens/BandsScreen';
+import colors from './colors/colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,8 +19,12 @@ export default function App() {
         <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}}/>
-            <Stack.Screen name="Bands" component={BandsScreen} options={({ route }) => ({ title: route.params.name })}/>
-
+            <Stack.Screen name="Bands" component={BandsScreen} options={({ route }) => (
+              { 
+                title: route.params.name ,
+                headerStyle: {backgroundColor: colors.primary},
+                headerTintColor: '#fff',
+              })}/>
           </Stack.Navigator>
         </NavigationContainer>
       </SafeAreaView>
