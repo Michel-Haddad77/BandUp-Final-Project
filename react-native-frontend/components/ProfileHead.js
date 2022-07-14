3202a7c3a59cdfcf1d0eaf6014b136b33a5386f5
Micho-PC <michelhdd27@gmail.com>
2022-07-14 13:33:22 +0300

    frontend: added a view container in profile head

--- react-native-frontend/components/ProfileHead.js
+++ react-native-frontend/components/ProfileHead.js
@@ -1,15 +1,18 @@
 import { View, StyleSheet, Button, Image, Text } from "react-native";
 import { styleProps } from "react-native-web/dist/cjs/modules/forwardedProps";
 
-function ProfileHead(props) {
+function ProfileHead() {
     return (
         <View style={styles.container}>
-            <Image style = {styles.image} source={require('../assets/profile2.png')}/>
-            <View style={styles.textContainer}>
-                <Text style={styles.title}>Full Name </Text>
-                <Text style={styles.subTitle}>Genre</Text>
-                <Text style={styles.subTitle}>Location</Text>
+            <View style={styles.container2}>
+                <Image style = {styles.image} source={require('../assets/profile2.png')}/>
+                <View style={styles.textContainer}>
+                    <Text style={styles.title}>Full Name </Text>
+                    <Text style={styles.subTitle}>Genre</Text>
+                    <Text style={styles.subTitle}>Location</Text>
+                </View>
             </View>
+            <Text style = {styles.description}> Lorem ipsum test test dshkja sajdhkja klasdlas</Text>
         </View>
     );
 }
@@ -18,6 +21,10 @@ export default ProfileHead;
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+    },
+
+    container2: {
         flex: 1,
         flexDirection: 'row',
         margin: 20,
@@ -39,5 +46,8 @@ const styles = StyleSheet.create({
     subTitle:{
         marginLeft: 5,
         fontSize: 20
+    },
+    description: {
+        flex: 1,
     }
 })
