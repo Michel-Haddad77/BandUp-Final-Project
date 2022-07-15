8ed08f523ff48fa5b4c437c7ebedc4eea898072d
Micho-PC <michelhdd27@gmail.com>
2022-07-16 01:16:56 +0300

    frontend: linked to login API

--- react-native-frontend/screens/LoginScreen.js
+++ react-native-frontend/screens/LoginScreen.js
@@ -1,21 +1,53 @@
+import { useState } from "react";
 import { StyleSheet, View, Image, Text, TextInput, } from "react-native";
 import StyledButton from "../components/StyledButton";
+import axios from 'axios';
+import url from "../constants/url";
 
-function LoginScreen(props) {
+function LoginScreen() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    //when user presses on Login button
+    function login(){
+        //linking with login api
+        axios({
+            method: 'post',
+            url: url + 'user/login',
+            data: {
+                email, 
+                password
+            }
+        }).then(function (response) {
+            console.log(response.data);
+        }).catch(function (error){
+            console.log(error.response.data);
+        })
+    }
+    
     return (
         <View style={styles.container}>
             <Image style={styles.image} source={require('../assets/logo.png')} />
              
             <Text style={styles.label}>Email</Text>
-            <TextInput style={styles.input} placeholder="jon@example.com"/>
+            <TextInput 
+                style={styles.input} 
+                placeholder="jon@example.com"
+                onChangeText={email => setEmail(email)}
+            />
 
             <Text style={styles.label}>Password</Text>
-            <TextInput secureTextEntry={true} style={styles.input} placeholder="password"/>
+            <TextInput secureTextEntry={true} 
+                style={styles.input} 
+                placeholder="password"
+                onChangeText={password => setPassword(password)}
+                />
 
             <StyledButton 
                 title="Login" 
                 text_style={styles.button_text} 
                 style={styles.button}
+                onPress= {login}
             />
 
             <Text style={styles.label2}>Late For The Party?</Text>
