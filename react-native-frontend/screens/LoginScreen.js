6a3aa781275e7eb4f3f15a6c9331233ea9927a2c
Micho-PC <michelhdd27@gmail.com>
2022-07-16 00:06:17 +0300

    frontend: added text inputs

--- react-native-frontend/screens/LoginScreen.js
+++ react-native-frontend/screens/LoginScreen.js
@@ -1,9 +1,17 @@
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, Text, TextInput, } from "react-native";
 
 function LoginScreen(props) {
     return (
-        <View>
+        <View style={styles.container}>
             <Image style={styles.image} source={require('../assets/logo.png')} />
+            <View style={styles.input_container}> 
+                <Text style={styles.label}>Email</Text>
+                <TextInput style={styles.input} placeholder="jon@example.com"/>
+
+                <Text style={styles.label}>Password</Text>
+                <TextInput secureTextEntry={true} style={styles.input} placeholder="password"/>
+            </View>
+        
         </View>
     );
 }
@@ -11,8 +19,27 @@ function LoginScreen(props) {
 export default LoginScreen;
 
 const styles = StyleSheet.create({
+    container:{
+       // alignItems: 'center',
+    },
+    label:{
+        fontSize: 20,
+        marginLeft: 10
+    },
+    input:{
+        borderWidth: 1,
+        borderColor: 'grey',
+        padding: 10,
+        borderRadius: 20,
+        marginBottom: 10,
+    },
+    input_container:{
+        marginHorizontal: 30,
+    },
     image:{
-        width: 150,
-        height: 150
+        width: 250,
+        height: 250,
+        marginTop: 20,
+        alignSelf: 'center'
     }
 })
