252543ce14d60e3f2f57bdfd2e3055d62cef7586
Micho-PC <michelhdd27@gmail.com>
2022-07-17 19:58:46 +0300

    frontend: added profile picture feature to register screen

--- react-native-frontend/screens/RegisterMusicianScreen.js
+++ react-native-frontend/screens/RegisterMusicianScreen.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View , TextInput} from 'react-native';
+import { StyleSheet, Text, View , TextInput, Image} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import StyledButton from '../components/StyledButton';
 import { useState,useEffect } from 'react';
 import axios from 'axios';
+import * as ImagePicker from 'expo-image-picker';
 
 import url from '../constants/url';
 
@@ -13,6 +14,9 @@ export default function RegisterMusicianScreen() {
     const [instrument, setInstrument] = useState("");
     const [description, setDescription] = useState("");
     const [instruments, setInstruments] = useState([]);
+    const [name, setName] = useState("");
+    const [last_name, setLastName] = useState("");
+    const [picture, setPicture] = useState("");
 
     useEffect(() => {
         axios({
@@ -26,9 +30,42 @@ export default function RegisterMusicianScreen() {
             console.log(error);
         })
     },[])
+
+    //function called when the user wants to upload an image
+    async function handleUpload(){
+        // No permissions request is necessary for launching the image library
+        //result= { cancelled: false, type: 'image', uri, width, height, base64 }
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [3, 3],
+            quality: 1,
+            base64: true, // result object will also contain a base 64 property
+        });
+
+        console.log(result);
+
+        if (!result.cancelled) {
+            setPicture(result.base64);
+        }
+    }
     
     return (
         <View style={styles.container}>
+            <Text style={styles.label}>First Name</Text>
+            <TextInput 
+                style={styles.input} 
+                placeholder="Jon"
+                onChangeText={name => setName(name)}
+            />
+
+            <Text style={styles.label}>Last Name</Text>
+            <TextInput 
+                style={styles.input} 
+                placeholder="Doe"
+                onChangeText={last_name => setLastName(last_name)}
+            />
+
             <Text style={styles.label}>Email</Text>
             <TextInput 
                 style={styles.input} 
@@ -58,18 +95,26 @@ export default function RegisterMusicianScreen() {
                     setInstrument(value)
                 }
             >
-                {//fill dropdown list according to number of instruments
-                    instruments.map((inst)=>
-                    <Picker.Item label={inst.instrument_name} value={inst._id} />
+                {   //fill dropdown list according to number of instruments
+                    instruments.map((inst, index)=>
+                    <Picker.Item key = {index} label={inst.instrument_name} value={inst._id} />
                 )}   
             </Picker>
 
+            <View style={styles.upload_container}>
+                {picture? (<Image source={{uri: `data:image;base64,${picture}`}} style={styles.image}/>): null}
+                <StyledButton 
+                    title="Upload Profile Picture" 
+                    text_style={styles.upload_button_text} 
+                    style={styles.upload_button}
+                    onPress={handleUpload}
+                />
+            </View>
+            
             <StyledButton 
                 title="Sign Up" 
-                text_style={styles.button_text} 
-                style={styles.button}
                 onPress={()=>{navigation.navigate('Register')}}
-            />
+            /> 
         </View>
     )
 }
@@ -89,4 +134,21 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         marginBottom: 10,
     },
+    upload_container:{
+        flexDirection: 'row',
+        marginBottom: 20,
+        justifyContent: 'space-evenly'
+    },
+    image:{
+        height: 60,
+        width: 60,
+        borderRadius: 50
+    },
+    upload_button:{
+        width: 130
+    },
+    upload_button_text:{
+        fontSize: 14,
+        textAlign: 'center'
+    }
 })
