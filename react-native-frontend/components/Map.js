f0cddf8c56e63e7337d82026ce565d354b0bf6e1
Micho-PC <michelhdd27@gmail.com>
2022-07-23 01:00:53 +0300

    frontend: map now shows nearby users markers

--- react-native-frontend/components/Map.js
+++ react-native-frontend/components/Map.js
@@ -3,38 +3,61 @@ import { View, Text,StyleSheet, Dimensions } from 'react-native';
 import MapView, { Callout, Marker } from 'react-native-maps';
 import StyledButton from './StyledButton';
 
-export default function Map() {
-    const [pin,setPin] = useState({
-        latitude: 37.78825,
-        longitude: 30.4324
+export default function Map({route}) {
+    const [center,setCenter] = useState({
+        latitude: 33.894757,
+        longitude: 35.482828
     });
+    const [nearby_users, setNearbyUsers] = useState([])
+
+
+    useEffect(() => {
+        //set the user's location as center
+        setCenter({
+            latitude: route.params.lat,
+            longitude: route.params.long
+        });
+
+        //get all the nearby users from route params
+        let users = route.params.nearby_users;
+        setNearbyUsers(users);
+        console.log(nearby_users);
+    }, [])
     
+
   return (
     <View>
         <MapView
             style={styles.map}
             initialRegion={{
-                latitude: 37.78825,
-                longitude: 30.4324,
+                latitude: center.latitude,
+                longitude: center.longitude,
                 latitudeDelta: 0.0922,
                 longitudeDelta: 0.0421,
             }}
         >
-            <Marker 
-                coordinate={pin}
-                draggable={true}
-                onDragEnd={(e)=>{
-                    console.log("loc=>", e.nativeEvent.coordinate);
-                    setPin({
-                        latitude: e.nativeEvent.coordinate.latitude,
-                        longitude: e.nativeEvent.coordinate.longitude
-                    })
-                }}   
-            >
+            {/* User's marker */}
+            <Marker coordinate={center}>
                 <Callout>
-                    <Text>Long Press to drag</Text>
+                    <Text>You</Text>
                 </Callout>
             </Marker>
+
+                {/* Nearby users markers */}
+                {nearby_users.map((user,index)=>
+                    <Marker 
+                        key={index}
+                        coordinate={{
+                            latitude: user.location.lat,
+                            longitude: user.location.long
+                        }}
+                        pinColor= {'blue'}
+                    >
+                        <Callout>
+                            <Text>{user.name}</Text>
+                        </Callout>
+                    </Marker>
+                )}
         </MapView>
     </View>
         
