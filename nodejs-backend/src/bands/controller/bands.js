6b8e2cd94b939068cc25c0aedff2cea153465eb2
Micho-PC <michelhdd27@gmail.com>
2022-07-22 22:18:47 +0300

    backend: created recent bands API

--- nodejs-backend/src/bands/controller/bands.js
+++ nodejs-backend/src/bands/controller/bands.js
@@ -81,6 +81,48 @@ async function getAllGenres(req,res){
 
 };
 
+async function getNearbyBands(req,res){
+    try{
+        // lat/long of the user
+        var lat1 = req.query.lat;
+        var long1 = req.query.long;
+
+        const bands = await Band.find().populate('genre');
+        var recent_bands = [];
+
+        bands.forEach((band)=>{
+            // lat/long for each band
+            var lat2 = band.location.lat;
+            var long2 = band.location.long;
+
+            //distance calculation in KM
+            var radlat1 = Math.PI * lat1/180;
+            var radlat2 = Math.PI * lat2/180;
+            var theta = long1-long2;
+            var radtheta = Math.PI * theta/180;
+            var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+            if (dist > 1) {
+                dist = 1;
+            }
+            dist = Math.acos(dist);
+            dist = dist * 180/Math.PI;
+            dist = dist * 111.2;
+
+            //if distance is less than 5KM
+            if (dist <= 5){
+                recent_bands.push(band);
+            }
+        })
+
+        console.log(recent_bands);
+        return res.send(recent_bands);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
+}
+
 module.exports = {
     getAllBands,
     getRecentBands,
