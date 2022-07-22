1be33422173387f63cbae0b6364ba64dc704aa44
Micho-PC <michelhdd27@gmail.com>
2022-07-22 23:47:44 +0300

    frontend: styling change

--- react-native-frontend/components/GenresInstrumentsSection.js
+++ react-native-frontend/components/GenresInstrumentsSection.js
@@ -74,6 +74,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'space-evenly',
+        marginBottom: 10
     },
     title: {
         fontSize: 21,
