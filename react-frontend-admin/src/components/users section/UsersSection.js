730a6363b2a5efeeeabc0769fc617c387ba0fdf6
Micho-PC <michelhdd27@gmail.com>
2022-07-24 00:44:51 +0300

    frontend: linking to get all musicians/bands api

--- react-frontend-admin/src/components/users section/UsersSection.js
+++ react-frontend-admin/src/components/users section/UsersSection.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserCard from '../user card/UserCard'
-import './styles.css'
+import './styles.css';
+import url from '../../constants/url';
+import axios from 'axios';
 
-export default function UsersSection() {
-  return (
-    <div className='section-container'>
-        <h2>Musicians</h2>
-        <UserCard/>
-    </div>
-  )
+export default function UsersSection({is_musician}) {
+    const [users,setUsers] = useState([]);
+
+    var url2 = '';
+
+    if (is_musician){
+        url2 = 'musicians/all';
+    }else{
+        url2 = 'bands/all'
+    }
+
+    useEffect(() => {
+        //get all musicians/bands
+        axios({
+            method: 'get',
+            url: url + url2,
+        }).then(function (response) {
+            console.log(response.data);
+            setUsers(response.data);
+        }).catch(function (error){
+            console.log(error);
+        })
+    }, [])
+    
+        
+    return (
+        <div className='section-container'>
+            <h2>{is_musician? "Musicians":"Bands"}</h2>
+
+            {users?
+                <div className='scrollable'>
+                    {users.map((user,index)=>(
+                        <UserCard
+                            key={index}
+                            name= {is_musician? `${user.name} ${user.last_name}` : user.name}
+                            picture={user.picture}
+                            genre_instrument = {is_musician? (user.instrument?.instrument_name) : (user.genre?.genre_name)}
+                        />
+                    ))}
+                </div>
+                : <h2>No Users Yet</h2>
+            }    
+        </div>
+    )
 }
--- react-frontend-admin/src/pages/home page/HomePage.js
+++ react-frontend-admin/src/pages/home page/HomePage.js
@@ -2,13 +2,13 @@ import SideBar from '../../components/side bar/SideBar';
 import './styles.css';
 import UsersSection from '../../components/users section/UsersSection';
 
-export default function LoginPage() {
+export default function HomePage() {
 
   return (
     <div className='page-container'>
       <SideBar />
       <div className='sections-container'>
-        <UsersSection/>
+        <UsersSection is_musician={true}/>
         <UsersSection/>
       </div>
     </div>
