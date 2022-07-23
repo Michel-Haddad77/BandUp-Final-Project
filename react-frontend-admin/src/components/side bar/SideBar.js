b52902708fc6fbd4e50fb8b72467bc1f76626612
Micho-PC <michelhdd27@gmail.com>
2022-07-23 23:01:11 +0300

    frontend: created all side bar buttons

--- react-frontend-admin/src/components/side bar/SideBar.js
+++ react-frontend-admin/src/components/side bar/SideBar.js
@@ -7,12 +7,31 @@ export default function SideBar() {
     
     return (
         <div className='sidebar'>
+        <div>
             <img  src={require('../../assets/logo.png')} className='logo'/>
             <h1 className='admin'>Administrator</h1>
+        </div>
             <button 
                 className='sidebar-btn' 
                 onClick={()=>navigate('/home')}
             >Users</button>
+
+            <button 
+                className='sidebar-btn' 
+                onClick={()=>navigate('/home')}
+            >Genres</button>
+
+            <button 
+                className='sidebar-btn' 
+                onClick={()=>navigate('/home')}
+            >Instruments</button>
+
+            <button 
+                className='sidebar-btn' 
+                onClick={()=>navigate('/home')}
+            >Logout</button>
+
+
         </div>
     )
 }
