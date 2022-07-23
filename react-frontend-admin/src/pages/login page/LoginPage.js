30a912af60d6b836d8394830ae3dbb71d5743b4e
Micho-PC <michelhdd27@gmail.com>
2022-07-23 15:34:52 +0300

    frontend: added global font

--- react-frontend-admin/src/pages/login page/LoginPage.js
+++ react-frontend-admin/src/pages/login page/LoginPage.js
@@ -5,15 +5,30 @@ export default function LoginPage() {
   return (
     <div className="login-container">
             <h3 className="login-h3">Login</h3>
-            <input 
-                type="text" 
-                placeholder="admin@bandup.com"
-                className='input'
-                onChange={(e) => {
-                    //setEmail(e.target.value);
-                }}
-                required
-            /><br/>
+            <div>
+              <label>Email</label><br/>
+              <input 
+                  type="text" 
+                  placeholder="admin@bandup.com"
+                  onChange={(e) => {
+                      //setEmail(e.target.value);
+                  }}
+                  required
+              /><br/>
+            </div>
+            
+            <div>
+              <label>Email</label><br/>
+              <input 
+                  type= 'password'
+                  placeholder="admin password"
+                  className='input'
+                  onChange={(e) => {
+                      //setEmail(e.target.value);
+                  }}
+                  required
+              /><br/>
+            </div>
     </div>
   )
 }
