9bf8b015b8486c8a5c9924279c904acce94e6c20
Micho-PC <michelhdd27@gmail.com>
2022-07-23 17:08:37 +0300

    frontend: created login function

---
+++ react-frontend-admin/src/constants/url.js
@@ -0,0 +1,3 @@
+const url = 'http://192.168.1.75:8080/api/';
+
+export default url;
--- react-frontend-admin/src/pages/login page/LoginPage.js
+++ react-frontend-admin/src/pages/login page/LoginPage.js
@@ -2,32 +2,69 @@ import React from 'react';
 import './styles.css';
 
 export default function LoginPage() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+    
+  //when user presses on Login button
+  async function login(){
+
+      //linking with login api
+      axios({
+          method: 'post',
+          url: url + 'user/login',
+          data: {
+              email, 
+              password
+          }
+      }).then(async function (response) {
+          console.log(response.data);
+          //store user token and info in async storage
+          try {
+              await AsyncStorage.setItem('token', response.data.token);
+              await AsyncStorage.setItem('user_info', JSON.stringify(response.data.user_info));
+              setToken(response.data.token);
+              setUser(response.data.user_info);
+          } catch(error) {
+              console.log(error);
+          }
+      }).catch(function (error){
+          console.log(error);
+      })
+  }
+
   return (
     <div className="login-container">
-            <h3 className="login-h3">Login</h3>
+            <h3 className="login-h3">Welcome Admin</h3>
             <div>
-              <label>Email</label><br/>
-              <input 
-                  type="text" 
-                  placeholder="admin@bandup.com"
-                  onChange={(e) => {
-                      //setEmail(e.target.value);
-                  }}
-                  required
-              /><br/>
-            </div>
-            
-            <div>
-              <label>Email</label><br/>
-              <input 
-                  type= 'password'
-                  placeholder="admin password"
-                  className='input'
-                  onChange={(e) => {
-                      //setEmail(e.target.value);
-                  }}
-                  required
-              /><br/>
+              <div>
+                <label>Email</label><br/>
+                <input 
+                    type="text" 
+                    placeholder="admin@bandup.com"
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    required
+                /><br/>
+              </div>
+              
+              <div>
+                <label>Password</label><br/>
+                <input 
+                    type= 'password'
+                    placeholder="admin password"
+                    className='input'
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    required
+                /><br/>
+              </div>
+
             </div>
     </div>
   )
