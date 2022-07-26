30d369f022d85c79c1c5b8085672ea272bb31f5f
Micho-PC <michelhdd27@gmail.com>
2022-07-26 18:00:16 +0300

    frontend: added functionality to search

--- react-native-frontend/screens/SearchScreen.js
+++ react-native-frontend/screens/SearchScreen.js
@@ -1,10 +1,64 @@
-import { View, Text, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, StyleSheet, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import url from '../constants/url';
+import { useAuthUser } from '../context/user';
+import UserCardHorizontal from '../components/UserCardHorizontal';
 
-export default function SearchScreen() {
+export default function SearchScreen({navigation}) {
+    const [users, setUsers] = useState([]);
+    const [filtered_users, setFilteredUsers] = useState([]);
+
+    const {user} = useAuthUser();
+
+    useEffect(() => {
+
+        if (user?.user_type === 1){
+            var url2 = 'musicians/all';
+        }else if(user?.user_type === 2){
+            var url2 = 'bands/all';
+        }
+
+        axios({
+            method: 'get',
+            url: url + url2,
+        }).then(function (response) {
+            setUsers(response.data);
+        }).catch(function (error){
+            console.log(error);
+        })
+    }, [])
+
+    function filter(text){
+        console.log(text);
+        if (text){
+            setFilteredUsers(users.filter((user)=>{
+                return user.name.includes(text)
+            }));
+            return
+        }
+        setFilteredUsers([])
+        console.log(filtered_users.length)
+    }
+    
   return (
     <View>
-      <TextInput  style={styles.searchbar} autoFocus={true} placeholder={"Search"}/>
+        <TextInput 
+            style={styles.searchbar} 
+            autoFocus={true} 
+            placeholder={"Search"}
+            onChangeText={(text)=>{filter(text)}}
+        />
+        <ScrollView>
+            {filtered_users && 
+                filtered_users.map((user, index)=> (
+                    <UserCardHorizontal  key = {index} 
+                    navigation = {navigation}
+                    displayed_user = {user}
+                />
+                ))}
+        </ScrollView>
+      
     </View>
   )
 }
