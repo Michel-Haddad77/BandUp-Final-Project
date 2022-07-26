82b5dcdc468e29d4d49e4fe64ed453420812a18c
Micho-PC <michelhdd27@gmail.com>
2022-07-26 15:15:20 +0300

    frontend: styled search bar

--- react-native-frontend/screens/SearchScreen.js
+++ react-native-frontend/screens/SearchScreen.js
@@ -1,10 +1,19 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, StyleSheet } from 'react-native'
 import React from 'react'
 
 export default function SearchScreen() {
   return (
     <View>
-      <TextInput autoFocus={true} placeholder={"Search"}/>
+      <TextInput  style={styles.searchbar} autoFocus={true} placeholder={"Search"}/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    searchbar:{
+        backgroundColor: 'white',
+        height: 50,
+        padding: 10,
+        fontSize: 18
+    }
+})
