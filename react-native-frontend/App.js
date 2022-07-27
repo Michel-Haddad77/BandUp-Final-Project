c6660f1139b1170a007b6bbd12d1d3e25f3d2f50
Micho-PC <michelhdd27@gmail.com>
2022-07-28 01:09:19 +0300

    frontend: notification now appears if the app is foregrounded

--- react-native-frontend/App.js
+++ react-native-frontend/App.js
@@ -1,16 +1,19 @@
-import { NavigationContainer} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
-
+import * as Notifications from 'expo-notifications';
 import colors from './constants/colors';
 import NavigationSwitcher from './navigation/NavigationSwitcher';
 import UserProvider from './context/user';
 
-
-const Stack = createNativeStackNavigator();
+//This will let the notification pop up when the app is foregrounded
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
 
