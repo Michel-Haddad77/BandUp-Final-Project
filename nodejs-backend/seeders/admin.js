d4b236ca428eb035fd34a6a20201929cff3d4e25
Micho-PC <michelhdd27@gmail.com>
2022-07-28 15:29:30 +0300

    backend: created function to seed the admin credentials

---
+++ nodejs-backend/seeders/admin.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+
+//establish connection to Mongodb because the seed files will be run seperately
+const DB_CONNECT = process.env.DB_CONNECT || "";
+
+mongoose.connect(
+    DB_CONNECT,
+    () => console.log('connected to db')
+);
+
+//seed the database with the admin data
+async function seedDB(){
+
+    try{
+        const salt = await bcrypt.genSalt(10);
+        const hash_password = await bcrypt.hash('123123', salt);
+
+       var admin = await User({
+        name: "Admin",
+        email: 'admin@bandup.com',
+        password: hash_password,
+        user_type: 0
+    });
+       await admin.save();
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//close connection to db
+seedDB().then(()=>{
+    console.log("The admin was seeded successfully");
+    mongoose.connection.close();
+})
