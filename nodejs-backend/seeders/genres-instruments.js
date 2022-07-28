f08a2b5f68d36c319fe78afc29adb3c380353afa
Micho-PC <michelhdd27@gmail.com>
2022-07-28 16:08:02 +0300

    backend: created seed file for default genres and instruments

---
+++ nodejs-backend/seeders/genres-instruments.js
@@ -0,0 +1,82 @@
+require("dotenv").config();
+const mongoose = require('mongoose');
+const Genre = require('../models/Genre');
+const Instrument = require('../models/Instrument');
+
+//establish connection to Mongodb because the seed files will be run seperately
+const DB_CONNECT = process.env.DB_CONNECT || "";
+
+mongoose.connect(
+    DB_CONNECT,
+    () => console.log('connected to db')
+);
+
+const fs = require('fs');
+
+//Genre Pics
+var hard_rock_pic = fs.readFileSync( __dirname + '/assets/hard-rock.jpg', 'base64');
+var rock_pic = fs.readFileSync( __dirname + '/assets/rock.jpg', 'base64');
+var metal_pic = fs.readFileSync( __dirname + '/assets/metal.jpg', 'base64');
+var jazz_pic = fs.readFileSync( __dirname + '/assets/jazz.jpg', 'base64');
+
+//Instrument Pics
+var guitar_pic = fs.readFileSync( __dirname + '/assets/guitar.jpg', 'base64');
+var singer_pic = fs.readFileSync( __dirname + '/assets/singer.jpg', 'base64');
+var drums_pic = fs.readFileSync( __dirname + '/assets/drummer.jpg', 'base64');
+var bass_pic = fs.readFileSync( __dirname + '/assets/bass.jpg', 'base64');
+
+const genres = [
+    {
+        genre_name: "Hard Rock",
+        picture: hard_rock_pic
+    },
+    {
+        genre_name: "Rock",
+        picture: rock_pic
+    },
+    {
+        genre_name: "Metal",
+        picture: metal_pic
+    },
+    {
+        genre_name: "Jazz",
+        picture: jazz_pic
+    },
+]
+
+const instruments = [
+    {
+        instrument_name: "Guiter",
+        picture: guitar_pic
+    },
+    {
+        instrument_name: "Singer",
+        picture: singer_pic
+    },
+    {
+        instrument_name: "Drums",
+        picture: drums_pic
+    },
+    {
+        instrument_name: "Bass",
+        picture: bass_pic
+    },
+]
+
+//seed the database with the default genres
+async function seedDB(){
+
+    try{
+        await Genre.insertMany(genres);
+        await Instrument.insertMany(instruments);
+       
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//close connection to db
+seedDB().then(()=>{
+    console.log("Genres and instruments were seeded successfully");
+    mongoose.connection.close();
+})
