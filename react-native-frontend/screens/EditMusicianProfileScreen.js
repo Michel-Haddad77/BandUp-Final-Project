82563fd0931d0c9e7107627bfdce3c56e0391405
Micho-PC <michelhdd27@gmail.com>
2022-07-30 13:12:44 +0300

    frontend: started edit profile screen for musicians

---
+++ react-native-frontend/screens/EditMusicianProfileScreen.js
@@ -0,0 +1,26 @@
+import { ScrollView, Text } from 'react-native'
+import React from 'react'
+
+export default function EditMusicianProfileScreen() {
+
+  //states
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [instrument, setInstrument] = useState("");
+  const [description, setDescription] = useState("");
+  const [instruments, setInstruments] = useState([]);
+  const [name, setName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [picture, setPicture] = useState("");
+  const [ location, setLocation] = useState({});
+  const [mobile, setMobile] = useState("")
+  
+  return (
+    <ScrollView style={styles.container}>
+      <MyTextInput label="FirstName"
+          placeholder="Jon"
+          onChangeText={name => setName(name)}
+      />
+    </ScrollView>
+  )
+}
