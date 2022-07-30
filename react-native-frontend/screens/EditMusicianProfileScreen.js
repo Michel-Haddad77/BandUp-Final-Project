8e5024ef4e9686358791643ed448a11bed1b10b3
Micho-PC <michelhdd27@gmail.com>
2022-07-30 21:57:56 +0300

    added edit profile picture

--- react-native-frontend/screens/EditMusicianProfileScreen.js
+++ react-native-frontend/screens/EditMusicianProfileScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, ScrollView, Text } from 'react-native'
+import { StyleSheet, ScrollView, Text, View, Image } from 'react-native'
 import { useState, useEffect } from 'react';
 import React from 'react'
 import ProfileTextInput from '../components/ProfileTextInput';
@@ -7,18 +7,20 @@ import colors from '../constants/colors';
 import {Picker} from '@react-native-picker/picker';
 import axios from 'axios';
 import url from '../constants/url';
+import StyledButton from '../components/StyledButton';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function EditMusicianProfileScreen() {
 
   const {user, setUser} = useAuthUser();
 
-  //states
-  const [instrument, setInstrument] = useState("");
+  //Initiate the states with the user's info
+  const [instrument, setInstrument] = useState(user.instrument._id);
   const [description, setDescription] = useState(user.description);
   const [instruments, setInstruments] = useState([]);
   const [name, setName] = useState(user.name);
   const [last_name, setLastName] = useState(user.last_name);
-  const [picture, setPicture] = useState("");
+  const [picture, setPicture] = useState(user.picture);
   const [ location, setLocation] = useState({});
   const [mobile, setMobile] = useState(user?.mobile)
 
@@ -35,46 +37,78 @@ export default function EditMusicianProfileScreen() {
     })
   },[])
 
+  //function called when the user wants to upload an image
+  async function handleUpload(){
+    //result= { cancelled: false, type: 'image', uri, width, height, base64 }
+    let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [3, 3],
+        quality: 1,
+        base64: true,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+        setPicture(result.base64);
+    }
+  }
+
   return (
     <ScrollView style={styles.container}>
+      <Image style = {styles.image} 
+        source={picture? {uri: `data:image;base64,${picture}`}: require('../assets/profile.png')}
+      />
+
+    <StyledButton 
+      title="Change Profile Picture"
+      style={styles.button}
+      text_style={styles.button_text}
+      onPress= {handleUpload}
+    />
+
       <ProfileTextInput label="First Name"
-          value={name}
-          onChangeText={name => setName(name)}
+        value={name}
+        onChangeText={name => setName(name)}
       />
 
       <ProfileTextInput label="Last Name"
-          value={last_name}
-          onChangeText={last_name => setLastName(last_name)}
+        value={last_name}
+        onChangeText={last_name => setLastName(last_name)}
       />
 
       <ProfileTextInput label="Description"
-          value={description}
-          onChangeText={description => setDescription(description)}
+        value={description}
+        onChangeText={description => setDescription(description)}
       />
 
       <ProfileTextInput label="Mobile Number"
-          keyboardType="phone-pad"
-          value={mobile}
-          onChangeText={mobile => setMobile(mobile)}
+        keyboardType="phone-pad"
+        value={mobile}
+        onChangeText={mobile => setMobile(mobile)}
       />
 
       <Text style={styles.label}>Instrument</Text>
-      <Picker
-          style={styles.input}
-          selectedValue={instrument}
-          onValueChange={(value,index) =>
-              setInstrument(value)
-          }
-      >
-          {   //fill dropdown list according to number of instruments
+      <View style={styles.picker_container}>
+        <Picker
+            style={styles.input}
+            selectedValue={instrument}
+            onValueChange={(value,index) =>
+                setInstrument(value)
+            }
+        >
+            { //fill dropdown list according to number of instruments
               instruments.map((inst, index)=>
               <Picker.Item style={styles.picker} 
                   key = {index} 
                   label={inst.instrument_name} 
                   value={inst._id} 
               />
-          )}   
-      </Picker>
+            )}   
+        </Picker>
+
+      </View>
     </ScrollView>
   )
 }
@@ -86,6 +120,27 @@ const styles = StyleSheet.create({
   label:{
     color: colors.secondary,
     marginTop: 10,
+  },
+  image: {
+    height:120,
+    width: 120,
+    borderRadius: 60,
+    alignSelf: 'center',
+    marginTop: 15
+  },
+  button:{
+    backgroundColor: 'rgba(0,0,0,0)',
+    elevation: 0,
+    
+  },
+  button_text:{
+    fontSize: 15,
+    color: colors.primary,
+    fontWeight: 'normal',
+  },
+  picker_container:{
+    borderBottomWidth:2,
+    borderColor: colors.primary,
   },
   picker:{
     fontSize:20,
