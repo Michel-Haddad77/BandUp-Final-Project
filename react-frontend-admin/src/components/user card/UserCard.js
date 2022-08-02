cd0555f4d739cdf55cf8ec3baa2976b3e45af601
Micho-PC <michelhdd27@gmail.com>
2022-08-02 14:49:00 +0300

    frontend: styling update for users section

--- react-frontend-admin/src/components/user card/UserCard.js
+++ react-frontend-admin/src/components/user card/UserCard.js
@@ -7,12 +7,19 @@ export default function UserCard({is_user, name, picture, genre_instrument, dele
 
   return (
     <div className='user-container'>
-        <img 
-          src={picture? `data:image;base64,${picture}`: require('../../assets/profile.png')} 
-          className="user-image"
-        />
-        <h3 className='name'>{name}</h3>
-        <h4 className='genre'>{genre_instrument}</h4>
+        <div className='flex-container'> 
+          <img 
+            src={picture? `data:image;base64,${picture}`: require('../../assets/profile.png')} 
+            className="user-image"
+          />
+        </div>
+        <div className='flex-container'>
+          <h3 className='name'>{name}</h3>
+        </div>
+        
+        <div className='flex-container'>
+          <h4 className='genre'>{genre_instrument}</h4>
+        </div>
         {is_user && <TiDelete className="delete-btn" onClick={deleteUser}/>}
         
     </div>
